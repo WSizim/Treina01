4ea2e748f9910e07fdd2e7920749a3e18901e378
wsizim <welson.sizim@gmail.com>
2016-02-06 16:17:05 -0200

    Release

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Registro()
+        {
+            var registro = new RegistroModel();
+
+            return View(registro);
+        }
+
+        [HttpPost]
+        public ActionResult Registro(RegistroModel registro)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Obrigado",registro);
+            }
+            else
+            {
+                return View(registro);
+            }
+        }
+
+        public ActionResult Obrigado(RegistroModel registro)
+        {
+            return View(registro);
+        }
+
+        public ActionResult Sobre()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ WebApplication1/Models/RegistroModel.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class RegistroModel
+    {
+        [Required(ErrorMessage = "Nome é Obrigatório!")]
+        [StringLength(100, MinimumLength = 5)]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Cidade é Obrigatório!")]
+        [StringLength(100, MinimumLength = 3)]
+        public string Cidade { get; set; }
+
+        [Required(ErrorMessage = "Estado é Obrigatório!")]
+        [StringLength(2, MinimumLength = 2)]
+        public string Estado { get; set; }
+
+        [Required(ErrorMessage = "Email é Obrigatório!")]
+        [StringLength(100, MinimumLength = 5)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O telefone deve ser informado.!")]
+        [DataType(DataType.PhoneNumber, ErrorMessage = "Forneça o número do telefone no formato (000) 0000-0000")]
+        [DisplayName("Número do Telefone")]
+        public string Fone { get; set; }
+
+        [Required(ErrorMessage = "O celular deve ser informado.!")]
+        [DataType(DataType.PhoneNumber, ErrorMessage = "Forneça o número do celular no formato (000) 00000-0000")]
+        [DisplayName("Número do Celular")]
+        public string Celular { get; set; }
+
+        [Required(ErrorMessage = "Descrição pessoal é Obrigatório!")]
+        [StringLength(100, MinimumLength = 5)]
+        public string Descricao { get; set; }
+    }
+}
